fda01947e994b716a99ce6dcc7b7496ce8b0cdd5
Alex <alex@gmail.com>
2025-02-25 00:05:52 +0200

    worked on slider hero

--- client/src/layouts/mainLayout/MainLayoutUI.tsx
+++ client/src/layouts/mainLayout/MainLayoutUI.tsx
@@ -2,6 +2,8 @@ import { FC, useState } from "react";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 import Sidebar from "../../components/Sidebar/Sidebar";
+import Hero from "../../components/Hero/Hero";
+import { useLocation } from "react-router-dom";
 
 type PropsType = {
   children: React.ReactNode;
@@ -9,11 +11,14 @@ type PropsType = {
 
 const MainLayoutUI: FC<PropsType> = ({ children }) => {
   const [sideOpen, setSideOpen] = useState<boolean>(false);
+  const location = useLocation();
 
   return (
     <div className="w-full min-h-screen flex flex-col items-center">
       <Header {...{ setSideOpen, sideOpen }} />
       <Sidebar {...{ sideOpen, setSideOpen }} />
+
+      {location.pathname === "/" && <Hero />}
       <div className="flex flex-col items-center w-full pad_page py-10 pb-[3000px]">
         {children}
       </div>
