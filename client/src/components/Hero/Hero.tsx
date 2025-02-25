8edd4302c9bf194580a3404fe2721b88c42a51a1
Alexander Matveev <matveevalexander470@gmail.com>
2025-02-25 13:23:16 +0200

    made breakpoints constants and intgrated in hero slider

--- client/src/components/Hero/Hero.tsx
+++ client/src/components/Hero/Hero.tsx
@@ -4,7 +4,8 @@ import { ChevronLeft, ChevronRight } from "lucide-react";
 import { useHero } from "./useHero";
 
 const Hero: FC = () => {
-  const { activeIndx, handleNext, handlePrev, setBtnClicked } = useHero();
+  const { activeIndx, handleNext, handlePrev, setBtnClicked, translateVal } =
+    useHero();
 
   return (
     <div className="pad_page w-full pt-5 flex flex-col justify-center">
@@ -19,16 +20,21 @@ const Hero: FC = () => {
           <ChevronLeft className="h-[50px] w-[50px] text-orange-500 btn__pseudo hover:scale-110" />
         </button>
 
-        <div className="w-full flex justify-items-center gap-5 overflow-x-auto hide_scrollbar snap-mandatory snap-x p-6 border-2 border-orange-500 rounded-xl">
+        <div className="w-full flex justify-items-center gap-[10%] overflow-x-auto hide_scrollbar snap-mandatory snap-x p-6 border-2 border-orange-500 rounded-xl">
           {heroFieldsArr.map((el) => (
-            <div key={el.id} className="w-full">
+            <div
+              key={el.id}
+              className="min-w-[75vw] sm:min-w-[450px] snap-center rounded-xl transition-all duration-500 overflow-hidden"
+              style={{
+                transform: `translateX(-${
+                  activeIndx * translateVal + activeIndx * 10
+                }%)`,
+              }}
+            >
               <img
                 src={el.img}
                 alt="burger_hero"
-                className="w-full object-cover h-fit min-w-[250px] sm:min-w-[450px] snap-center rounded-xl transition-all duration-500"
-                style={{
-                  transform: `translateX(-${activeIndx * 100}%)`,
-                }}
+                className="w-full object-cover h-fit "
               />
             </div>
           ))}
