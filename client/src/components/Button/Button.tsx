9d89d7e546f2a42ba2a221675f13c6c81142bf6f
Alexander Matveev <matveevalexander470@gmail.com>
2025-02-25 23:47:06 +0200

    started loader

---
+++ client/src/components/Button/Button.tsx
@@ -0,0 +1,40 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { FC } from "react";
+import { Beef, Cookie, Fish, Ham, IceCreamCone, Pizza } from "lucide-react";
+
+type PropsType = {
+  isLoading?: boolean;
+  isDisabled?: boolean;
+  label?: string;
+  handleClick?: (...params: any) => void;
+};
+
+const Button: FC<PropsType> = ({
+  isLoading,
+  isDisabled,
+  label,
+  handleClick,
+}) => {
+  return (
+    <div className="btn_container">
+      <div className="btn_container__content">
+        <button disabled={isDisabled} className="content__btn">
+          <span className="btn__txt">{label ?? "BUTTON"}</span>
+        </button>
+
+        <span className="btn__ref_1"></span>
+        <span className="btn__ref_2"></span>
+      </div>
+
+      <span className="btn_container__shadow"></span>
+
+      <Pizza className="btn_container__svg_1" />
+      <Ham className="btn_container__svg_2" />
+      <Beef className="btn_container__svg_3" />
+      <Fish className="btn_container__svg_4" />
+      <Cookie className="btn_container__svg_5" />
+      <IceCreamCone className="btn_container__svg_6" />
+    </div>
+  );
+};
+export default Button;
