04e6b944c23f82a0ecd7025e9fd2784991bbe0c5
Alexander Matveev <matveevalexander470@gmail.com>
2025-02-26 20:07:36 +0200

    made dropdown static or absolute depending on props

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -4,10 +4,10 @@ const Home: FC = () => {
   return (
     <div className="w-full flex flex-col items-center gap-y-5">
       <div className="w-full flex flex-col items-start gap-y-3">
-        <span className="txt__04 text-orange-500">
+        <span className="txt__04 text-[whitesmoke]">
           Order Comfortably From Home
         </span>
-        <span className="txt__04 text-orange-500">
+        <span className="txt__04 text-[whitesmoke]">
           Or Manage Your Business From Anywhere
         </span>
       </div>
