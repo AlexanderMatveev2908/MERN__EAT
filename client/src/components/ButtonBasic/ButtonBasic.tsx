5b51c85e1c8ef4bea8512f86e5c217716fe9f2e0
Alexander Matveev <matveevalexander470@gmail.com>
2025-02-26 14:10:28 +0200

    worked on popup, decided to use as component instead of creating state in context

---
+++ client/src/components/ButtonBasic/ButtonBasic.tsx
@@ -0,0 +1,38 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { FC } from "react";
+
+type PropsType = {
+  isLoading?: boolean;
+  isDisabled?: boolean;
+  label?: string;
+  type?: "button" | "submit" | "reset";
+  styleTxt?: string;
+  styleBtn?: string;
+  handleClick?: (...params: any) => void;
+};
+
+const ButtonBasic: FC<PropsType> = ({
+  isLoading,
+  isDisabled,
+  label,
+  type = "button",
+  handleClick,
+  styleTxt,
+  styleBtn,
+}) => {
+  return (
+    <button
+      disabled={isDisabled || isLoading}
+      onClick={handleClick}
+      type={type}
+      className="btn__with_shadow group "
+    >
+      <div className={`${styleBtn} btn__with_shadow__content`}>
+        <span className={`${styleTxt} content__txt`}>{label}</span>
+      </div>
+
+      <span className={`${styleBtn} btn__with_shadow__shadow`}></span>
+    </button>
+  );
+};
+export default ButtonBasic;
