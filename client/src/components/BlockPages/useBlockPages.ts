884e52a4c001d8b09579228a5b11f7ba41adf5df
Alexander Matveev <matveevalexander470@gmail.com>
2025-02-27 09:05:40 +0200

    added functionality of mouseDown and mouseUp with interval for blockPages switch

--- client/src/components/BlockPages/useBlockPages.ts
+++ client/src/components/BlockPages/useBlockPages.ts
@@ -1,15 +1,16 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { tailwindBreak } from "../../constants/breakpoints";
 
 export const useBlockPages = ({ totPages }: { totPages: number }) => {
   const [blockSize, setBlockSize] = useState<number>(3);
   const [currBlock, setCurrBlock] = useState<number>(1);
+  const intervalRef = useRef<NodeJS.Timeout | null>(null);
 
   useEffect(() => {
     const updateBlockSize = () =>
       setBlockSize(
         window.innerWidth > tailwindBreak._2xl
-          ? 15
+          ? 12
           : window.innerWidth > tailwindBreak.lg
           ? 10
           : window.innerWidth > tailwindBreak.md
@@ -40,13 +41,75 @@ export const useBlockPages = ({ totPages }: { totPages: number }) => {
   const handlePrev = () =>
     isPrevDisabled ? null : setCurrBlock(currBlock - 1);
 
+  const startIntervalHandler = (action: "prev" | "next") => {
+    if (intervalRef.current) return;
+
+    intervalRef.current = setInterval(() => {
+      setCurrBlock((prev) => {
+        const newBlock = action === "prev" ? prev - 1 : prev + 1;
+
+        const isPrevDisabled = newBlock < 1;
+        const isNextDisabled = (newBlock - 1) * blockSize >= totPages!;
+
+        if (
+          (action === "prev" && isPrevDisabled) ||
+          (action === "next" && isNextDisabled)
+        ) {
+          clearIntervalHandler();
+          return prev;
+        }
+
+        return newBlock;
+      });
+    }, 150);
+  };
+
+  const handlePrevInterval = () => {
+    if (intervalRef.current) return;
+    intervalRef.current = setInterval(() => {
+      setCurrBlock((prev) => {
+        if (prev === 1) {
+          clearIntervalHandler();
+          return prev;
+        }
+        return prev - 1;
+      });
+    }, 150);
+  };
+
+  const handleNextInterval = () => {
+    if (intervalRef.current) return;
+    intervalRef.current = setInterval(() => {
+      setCurrBlock((prev) => {
+        if (prev * blockSize >= totPages!) {
+          clearIntervalHandler();
+          return prev;
+        }
+        return prev + 1;
+      });
+    }, 150);
+  };
+
+  const clearIntervalHandler = () => {
+    if (intervalRef.current) {
+      clearInterval(intervalRef.current);
+      intervalRef.current = null;
+    }
+  };
+
+  useEffect(() => {
+    return () => clearIntervalHandler();
+  }, []);
+
   return {
-    blockEls,
     handleNext,
     handlePrev,
     isPrevDisabled,
     isNextDisabled,
-    blockSize,
     arrToMakeBtns,
+    startIntervalHandler,
+    handlePrevInterval,
+    handleNextInterval,
+    clearIntervalHandler,
   };
 };
