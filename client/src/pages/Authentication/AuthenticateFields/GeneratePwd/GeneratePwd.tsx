ab7c81780d6dc035308e73b9daf022dc6e2faa9a
Alexander Matveev <matveevalexander470@gmail.com>
2025-03-01 14:13:53 +0200

    adjusted animation cpy pwd

--- client/src/pages/Authentication/AuthenticateFields/GeneratePwd/GeneratePwd.tsx
+++ client/src/pages/Authentication/AuthenticateFields/GeneratePwd/GeneratePwd.tsx
@@ -3,14 +3,14 @@ import { FC } from "react";
 import { useGeneratePwd } from "./useGeneratePwd";
 
 const GeneratePwd: FC = () => {
-  const { strongPwd, handlePwdBtn, handleCopyBnt, isCopyingTooltip } =
+  const { strongPwd, generatePwd, handleCopyPwd, tooltipRef } =
     useGeneratePwd();
 
   return (
     <div className="w-full max-w-full grid grid-cols-1 gap-3">
       <div className="w-full max-w-fit flex hover:text-orange-500 btn__pseudo">
         <button
-          onClick={handlePwdBtn}
+          onClick={generatePwd}
           type="button"
           className="justify-self-start flex gap-4 items-center cursor-pointer"
         >
@@ -24,15 +24,15 @@ const GeneratePwd: FC = () => {
         <div className="relative group w-full flex max-w-fit">
           <button
             type="button"
-            onClick={handleCopyBnt}
+            ref={tooltipRef}
+            onClick={handleCopyPwd}
             className="txt__00 btn__pseudo border-2 border-orange-500 rounded-xl w-full max-w-fit px-6 py-1 cursor-pointer hover:text-orange-500 break-all text-start"
           >
             {strongPwd}
           </button>
           <span
-            className={`tooltip txt__00 max-w-fit px-6 min-w-[150px] ${
-              isCopyingTooltip ? "generate_password__tooltip" : "opacity-0"
-            }`}
+            id="tooltip"
+            className="tooltip txt__00 max-w-fit px-6 min-w-[150px] opacity-0"
           >
             Password Copied
           </span>
