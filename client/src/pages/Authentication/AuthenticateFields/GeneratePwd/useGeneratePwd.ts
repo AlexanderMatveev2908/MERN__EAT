ae4ee0e4db0bb36d594e32b656a3868d72cb3d31
Alex <alex@gmail.com>
2025-03-01 10:19:31 +0200

    implememnted basic tooltips, in mobile and sm with animation copy txt

--- client/src/pages/Authentication/AuthenticateFields/GeneratePwd/useGeneratePwd.ts
+++ client/src/pages/Authentication/AuthenticateFields/GeneratePwd/useGeneratePwd.ts
@@ -1,13 +1,28 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { REG_PWD } from "../../../../constants/regex";
+import { tailwindBreak } from "../../../../constants/breakpoints";
 
 export const useGeneratePwd = () => {
+  const genRef = useRef<HTMLButtonElement | null>(null);
   const [strongPwd, setStrongPwd] = useState<string>("");
+  const [isGeneratePwdTooltip, setIsGeneratePwdTooltip] = useState(false);
+  const [isCopyingTooltip, setIsCopyingTooltip] = useState(false);
+
+  useEffect(() => {
+    const handleToolRef = (e: MouseEvent) => {
+      if (genRef.current && !genRef.current?.contains(e.target as Node))
+        setIsGeneratePwdTooltip(false);
+    };
+
+    document.addEventListener("mousedown", handleToolRef);
+
+    return () => document.removeEventListener("mousedown", handleToolRef);
+  }, []);
 
   const getRandomVals = () => {
     const charSet =
       "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+{}[]";
-    const arrBytes = new Uint8Array(16);
+    const arrBytes = new Uint8Array(24);
     window.crypto.getRandomValues(arrBytes);
 
     return Array.from(arrBytes, (el) => charSet[el % charSet.length]).join("");
@@ -27,9 +42,28 @@ export const useGeneratePwd = () => {
     if (strongPwd) navigator.clipboard.writeText(strongPwd);
   };
 
+  const handlePwdBtn = () => {
+    generatePwd();
+    if (window.innerWidth < tailwindBreak.md) {
+      setIsGeneratePwdTooltip(!isGeneratePwdTooltip);
+      setTimeout(() => setIsGeneratePwdTooltip(false), 750);
+    }
+  };
+
+  const handleCopyBnt = () => {
+    handleCopyPwd();
+    if (window.innerWidth < tailwindBreak.md) {
+      setIsCopyingTooltip(true);
+      setTimeout(() => setIsCopyingTooltip(false), 750);
+    }
+  };
+
   return {
-    generatePwd,
+    handlePwdBtn,
+    handleCopyBnt,
+    isGeneratePwdTooltip,
+    isCopyingTooltip,
     strongPwd,
-    handleCopyPwd,
+    genRef,
   };
 };
