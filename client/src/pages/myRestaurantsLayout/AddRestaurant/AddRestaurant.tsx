0a668d6a6d00db10ff98d07506aee2bf0517a4d6
Alexander Matveev <matveevalexander470@gmail.com>
2025-03-11 16:01:00 +0200

    i logged the processed formData in which i append values given by useFormHook, it seems ok, i start create backend route create myRestaurant

--- client/src/pages/myRestaurantsLayout/AddRestaurant/AddRestaurant.tsx
+++ client/src/pages/myRestaurantsLayout/AddRestaurant/AddRestaurant.tsx
@@ -1,17 +1,18 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { FC } from "react";
-import { useAddRestaurant } from "./hooks/useAddRestaurant";
+import { useAddRestaurant } from "./useAddRestaurant";
 import { FormProvider } from "react-hook-form";
 import MyRestaurantsForm from "../../../forms/MyRestaurants/MyRestaurantsForm";
 
 const AddRestaurant: FC = () => {
-  const { formContext } = useAddRestaurant();
+  const { formContext, handleSave } = useAddRestaurant();
 
   return (
     <FormProvider {...formContext}>
       <div className="w-full grid grid-cols-1 justify-items-center gap-y-5">
         <span className="txt__04">Create new restaurant</span>
 
-        <MyRestaurantsForm {...{ formContext }} />
+        <MyRestaurantsForm {...({ formContext, handleSave } as any)} />
       </div>
     </FormProvider>
   );
---
+++ client/src/utils/prepareFormDataRestaurants.ts
@@ -0,0 +1,24 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { MyRestaurantsAddUpdateFormType } from "../types/myRestaurants";
+
+export const prepareFormData = (
+  data: MyRestaurantsAddUpdateFormType
+): FormData => {
+  const formData = new FormData();
+
+  const { images, categories, ...primitiveVals } = data;
+
+  [...(images as any)].forEach((img) =>
+    formData.append("imagesRestaurant", img)
+  );
+  categories.forEach((cat) => formData.append("categories", cat));
+
+  for (const key in primitiveVals) {
+    formData.append(
+      key,
+      primitiveVals[key as keyof MyRestaurantsAddUpdateFormType]
+    );
+  }
+
+  return formData;
+};
