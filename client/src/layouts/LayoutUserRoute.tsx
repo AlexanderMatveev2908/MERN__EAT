592aa7cbe2aea0eec79a3af228aa2e274ff0b476
Alex <alex@gmail.com>
2025-03-12 13:27:28 +0200

    modified path push non logged user from authlogin to /

--- client/src/layouts/LayoutUserRoute.tsx
+++ client/src/layouts/LayoutUserRoute.tsx
@@ -4,6 +4,6 @@ import { Navigate, Outlet } from "react-router-dom";
 
 const LayoutUserRoute: FC = () => {
   const { isLogged } = useUser();
-  return isLogged ? <Outlet /> : <Navigate to="/auth/login" replace />;
+  return isLogged ? <Outlet /> : <Navigate to="/" replace />;
 };
 export default LayoutUserRoute;
