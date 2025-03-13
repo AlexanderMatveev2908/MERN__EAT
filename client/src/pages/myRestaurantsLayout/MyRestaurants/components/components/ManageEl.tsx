f2243d647684dd5703077e13e21b1d41d9bacef2
Alexander Matveev <matveevalexander470@gmail.com>
2025-03-13 10:54:58 +0200

    switched to ES6 in backend in ts target

--- client/src/pages/myRestaurantsLayout/MyRestaurants/components/components/ManageEl.tsx
+++ client/src/pages/myRestaurantsLayout/MyRestaurants/components/components/ManageEl.tsx
@@ -14,17 +14,17 @@ type PropsType = {
 
 const ManageEl: FC<PropsType> = ({ el }) => {
   return (
-    <div className="w-full grid grid-cols-[150px_20px_1fr] items-center py-1">
+    <div className="w-full grid grid-cols-[120px_50px_1fr] items-center py-1">
       <div className="flex w-full items-center gap-3">
-        <el.icon className="w-[30px] h-[30px]" />
+        <el.icon className="min-w-[30px] min-h-[30px]" />
         <span className="txt__02">{el.label}</span>
       </div>
 
-      <span className="txt__02 text-center ">{el.val}</span>
+      <span className="txt__02 text-center">{el.val}</span>
 
       <Link
         to="/"
-        className="w-full max-w-fit txt__02 justify-self-end transition-all duration-300 cursor-pointer hover:text-orange-500 hover:scale-110 border-2 rounded-xl px-12 py-1 border-orange-500"
+        className="txt__02 justify-self-end transition-all duration-300 cursor-pointer hover:text-orange-500 hover:scale-110 border-2 rounded-xl py-1 border-orange-500 w-[90%] text-center"
       >
         View
       </Link>
