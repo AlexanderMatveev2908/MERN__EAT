f84a54ed9031d7aa13df543a4dfb9a54d4fbea7e
Alex <alex@gmail.com>
2025-03-13 13:18:17 +0200

    _

---
+++ client/src/config/onlyDev/defVals.ts
@@ -0,0 +1,17 @@
+export const defaultValuesMyRest = {
+  name: "Name",
+  country: "Country",
+  state: "State",
+  city: "City",
+  street: "Street",
+  zipCode: "12345",
+  email: "email@exampple.com",
+  phone: "1234567890",
+  website: "http://www.example.com",
+  estTimeDelivery: "30",
+  price: "10",
+  freeDeliveryPrice: "50",
+  categories: ["italian", "chinese"],
+  openTime: "480",
+  closeTime: "1200",
+};
