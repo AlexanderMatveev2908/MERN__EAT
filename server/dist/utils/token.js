9bf8f875ec87c6f4eedfdabdd0e8f9132f6a0093
Alexander Matveev <matveevalexander470@gmail.com>
2025-03-13 09:24:22 +0200

    _

--- server/dist/utils/token.js
+++ server/dist/utils/token.js
@@ -1,4 +1,37 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || (function () {
+    var ownKeys = function(o) {
+        ownKeys = Object.getOwnPropertyNames || function (o) {
+            var ar = [];
+            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
+            return ar;
+        };
+        return ownKeys(o);
+    };
+    return function (mod) {
+        if (mod && mod.__esModule) return mod;
+        var result = {};
+        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
+        __setModuleDefault(result, mod);
+        return result;
+    };
+})();
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -15,9 +48,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.checkTokenJWE = exports.genTokenJWE = exports.decodeExpiredJWT = exports.verifyAccessJWT = exports.genAccessJWT = exports.checkTokenSHA = exports.genTokenSHA = void 0;
 const crypto_1 = __importDefault(require("crypto"));
 const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
-const jose_1 = require("jose");
 const keys_1 = require("./keys");
 const signs_1 = require("../config/signs");
+const loadJose = () => __awaiter(void 0, void 0, void 0, function* () {
+    const { CompactEncrypt, jwtDecrypt } = yield Promise.resolve().then(() => __importStar(require("jose")));
+    return { CompactEncrypt, jwtDecrypt };
+});
 const genTokenSHA = (type) => {
     const token = crypto_1.default.randomBytes(64).toString("hex");
     const hashedToken = crypto_1.default
@@ -60,7 +96,8 @@ const genTokenJWE = (userId) => __awaiter(void 0, void 0, void 0, function* () {
         userId,
         token,
     };
-    const jwe = yield new jose_1.CompactEncrypt(new TextEncoder().encode(JSON.stringify(payload)))
+    const { CompactEncrypt } = yield loadJose();
+    const jwe = yield new CompactEncrypt(new TextEncoder().encode(JSON.stringify(payload)))
         .setProtectedHeader({ alg: "RSA-OAEP", enc: "A256GCM" })
         .encrypt(publicKey);
     const expiry = (0, signs_1.GEN_EXPIRY_REFRESH)();
@@ -79,7 +116,8 @@ const checkTokenJWE = (tokenJWE) => __awaiter(void 0, void 0, void 0, function* 
         privateKey = keys.cryptoPrivateKey;
     }
     try {
-        const { payload } = yield (0, jose_1.jwtDecrypt)(tokenJWE, privateKey);
+        const { jwtDecrypt } = yield loadJose();
+        const { payload } = yield jwtDecrypt(tokenJWE, privateKey);
         return payload;
     }
     catch (err) {
