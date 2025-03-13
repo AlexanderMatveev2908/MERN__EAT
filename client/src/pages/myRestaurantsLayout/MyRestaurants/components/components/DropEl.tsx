18031c2fd1aef486681e57df41535e3d9f4f2e9a
Alexander Matveev <matveevalexander470@gmail.com>
2025-03-13 06:50:52 +0200

    in my opinion seems more clean and funny witouth compromising practical goal of menu to show data

--- client/src/pages/myRestaurantsLayout/MyRestaurants/components/components/DropEl.tsx
+++ client/src/pages/myRestaurantsLayout/MyRestaurants/components/components/DropEl.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { ChevronDown } from "lucide-react";
-import { FC, useState } from "react";
+import { ChevronUp } from "lucide-react";
+import { FC, useRef, useState } from "react";
 
 type PropsType = {
   el: {
@@ -12,20 +12,33 @@ type PropsType = {
 
 const DropEl: FC<PropsType> = ({ el }) => {
   const [isOpen, setIsOpen] = useState(false);
+  const timerRef = useRef<NodeJS.Timeout | null>(null);
+
+  const handleMouseEnter = () => {
+    if (timerRef.current) clearTimeout(timerRef.current);
+
+    setIsOpen(true);
+  };
+
+  const handleMouseLeave = () => {
+    timerRef.current = setTimeout(() => {
+      setIsOpen(false);
+    }, 250);
+  };
 
   return (
     <div className="w-full grid grid-cols-1 transition-all duration-300 relative">
       <div
         onClick={() => setIsOpen(!isOpen)}
-        className={`w-full border-t-2 border-b-2 ${
-          isOpen ? "border-b-orange-500" : "border-b-transparent"
-        } border-orange-500 flex justify-between items-center px-3 py-1 group cursor-pointer transition-all duration-300`}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+        className={`w-full flex justify-between items-center px-3 py-1 group cursor-pointer transition-all duration-300`}
       >
         <div className=" transition-all duration-300 group-hover:text-orange-500 flex gap-3 items-center">
           <el.icon className="w-[25px] h-[25px]" />
           <span className="txt__02">{el.label}</span>
         </div>
-        <ChevronDown
+        <ChevronUp
           className={`w-[35px] h-[35px] transition-all duration-300 group-hover:text-orange-500 ${
             isOpen ? "rotate-180" : ""
           }`}
@@ -33,20 +46,20 @@ const DropEl: FC<PropsType> = ({ el }) => {
       </div>
 
       <ul
-        className={`w-full grid py-1 transition-all duration-300 ${
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+        className={`w-[80%] right-0 max-h-fit grid py-1 border-2 border-orange-500 rounded-xl bg-[#111] transition-all duration-500 absolute  ${
           el.label === "Categories" ? "grid-cols-2" : " grid-cols-1 gap-1"
         } ${
           isOpen
-            ? "pointer-events-auto opacity-100"
-            : "-translate-y-[100px] pointer-events-none opacity-0 absolute top-0 left-0"
+            ? "opacity-100 -translate-y-full pointer-events-auto"
+            : "translate-y-full opacity-0 pointer-events-none"
         }`}
       >
         {el.vals.map((val: string | number, i) => (
           <li
             key={i}
-            className={`px-3 transition-all duration-300 truncate max-w-full ${
-              isOpen ? "opacity-100" : "opacity-0"
-            }`}
+            className="px-3 transition-all duration-300 truncate hover:text-orange-500"
           >
             <span className="txt__01">{val}</span>
           </li>
