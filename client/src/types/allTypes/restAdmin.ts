c4c6f46549e996c9a2f42043a63af37116f969a3
Alexander Matveev <matveevalexander470@gmail.com>
2025-03-17 14:43:57 +0200

    finifhed card

--- client/src/types/allTypes/restAdmin.ts
+++ client/src/types/allTypes/restAdmin.ts
@@ -59,6 +59,7 @@ export type MyRestaurantType = {
   reviews?: ReviewType[];
 
   dishesCount: number;
+  avgPrice: number;
 
   pendingOrders: number;
   processingOrders: number;
--- client/src/utils/allUtils/priceFormatter.ts
+++ client/src/utils/allUtils/priceFormatter.ts
@@ -1,7 +1,16 @@
-export const priceFormatter = (price: number) =>
-  price
-    ? new Intl.NumberFormat("en-US", {
-        style: "currency",
-        currency: "USD",
-      }).format(price)
-    : "Free";
+export const priceFormatter = ({
+  price,
+  showStr = false,
+}: {
+  price: number;
+  showStr?: boolean;
+}) => {
+  const formatted = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  }).format(price ?? 0);
+
+  if (!price && showStr) return "Free";
+
+  return formatted;
+};
