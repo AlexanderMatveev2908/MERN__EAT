ffc72a3cfcc19282413ee99b528878553cc79e99
Alexander Matveev <matveevalexander470@gmail.com>
2025-03-22 15:35:50 +0200

    add timeout to focus of forms async and set ref for async callback in verify pages that runs coditionally on query

--- client/src/pages/userLayout/UserProfile/hooks/useUserProfile.ts
+++ client/src/pages/userLayout/UserProfile/hooks/useUserProfile.ts
@@ -1,4 +1,4 @@
-import { RefObject, useEffect, useRef } from "react";
+import { RefObject, useCallback, useEffect, useRef } from "react";
 import { useProfileReducer } from "./UseProfileReducer/useProfileReducer";
 
 export type InputRefType = RefObject<HTMLInputElement | null>;
@@ -18,19 +18,26 @@ export const useUserProfile = () => {
     handleSubmit,
   } = useProfileReducer();
 
-  useEffect(() => {
-    const makeFocus = () => {
-      const refs = [inputRef_0, inputRef_1, inputRef_2];
+  const makeFocus = useCallback(() => {
+    const refs = [inputRef_0, inputRef_1, inputRef_2];
 
-      const i = state.currForm.curr;
-      if (refs[i].current)
-        setTimeout(() => {
-          refs[i].current?.focus();
-        }, 600);
-    };
-
-    makeFocus();
+    const i = state.currForm.curr;
+    if (refs[i].current)
+      setTimeout(() => {
+        refs[i].current?.focus();
+      }, 600);
   }, [state.currForm.curr]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      makeFocus();
+    }, 200);
+    // eslint-disable-next-line
+  }, []);
+
+  useEffect(() => {
+    makeFocus();
+  }, [makeFocus]);
 
   const { isPrevDisabled, isNextDisabled, curr } = state.currForm;
 
