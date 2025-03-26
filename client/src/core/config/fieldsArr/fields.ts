bab58cbb7037026073be781855d71c2ad0477ab5
Alexander Matveev <matveevalexander470@gmail.com>
2025-03-26 22:47:10 +0200

    _

--- client/src/core/config/fieldsArr/fields.ts
+++ client/src/core/config/fieldsArr/fields.ts
@@ -1,76 +1,12 @@
-export { makeLinksMyRestPage } from "./allFields/MySingleRestaurant/show";
-
-export {
-  fieldsShowMyRestaurants,
-  makeSubFieldsOrders,
-  makeSubFieldsReviews,
-  showFieldDishes,
-  showFieldOrders,
-  showFieldReviews,
-  showMyRestaurantsDelivery,
-  showMyRestaurantsDeliveryFields,
-  showMyRestaurantsOpenHours,
-  showMyRestaurantsOpenHoursFields,
-} from "./allFields/MyRestaurants/show";
-
-export { switchFormAuthRight } from "./allFields/switchForms";
-
-export {
-  userProfileFields_0,
-  userProfileFields_1,
-  userProfileFields_2,
-  allFields,
-  fieldsDividedByArea,
-  totLenUserDetails,
-} from "./allFields/userDetailsFields";
-
-export { switchFormAuthLeft } from "./allFields/switchForms";
-
-export {
-  passwordCheckerFieldsArr,
-  REG_LOWERCASE,
-  REG_NUMBER,
-  REG_SYMBOL,
-  REG_UPPERCASE,
-} from "./allFields/pwdFieldsRegChecker";
-
-export { heroFieldsArr, heroLorem } from "./allFields/heroFieldsArr";
-
-export { footerFieldsArr, socialFieldsArr } from "./allFields/footerFields";
-
-export {
-  changeEmailField,
-  confirmPwdField,
-  emailField,
-  nameFieldsArr,
-  pwdFieldToAccess,
-  pwdFieldToChangeOldPwd,
-  pwdFieldToCreate,
-} from "./allFields/authFieldsUser";
-
-export {
-  allowedImgs,
-  arrCatByArea,
-  myRestaurantsAddress_0,
-  myRestaurantsAddress_1,
-  myRestaurantsAddressByArea,
-  myRestaurantsContact,
-  myRestaurantsDeliveryFields,
-  myRestaurantsName,
-  myRestaurantsOpenCloseFields,
-  totLenAddressMyRestaurants,
-  totLenMyRestaurantsCat,
-} from "./allFields/MyRestaurants/makeUpdate";
-
-export {
-  myRestSorters,
-  myRestFilters,
-  myRestFieldsSearch,
-} from "./allFields/MyRestaurants/filterSort";
-
-export {
-  allUsersFields,
-  loggedUserFields,
-  loggedUserFieldsDrop,
-  nonLoggedUserFields,
-} from "./allFields/dropSideFields";
+export * from "./allFields/MySingleRestaurant/show";
+export * from "./allFields/MyRestaurants/show";
+export * from "./allFields/switchForms";
+export * from "./allFields/userDetailsFields";
+export * from "./allFields/switchForms";
+export * from "./allFields/pwdFieldsRegChecker";
+export * from "./allFields/heroFieldsArr";
+export * from "./allFields/footerFields";
+export * from "./allFields/authFieldsUser";
+export * from "./allFields/MyRestaurants/makeUpdate";
+export * from "./allFields/MyRestaurants/filterSort";
+export * from "./allFields/dropSideFields";
