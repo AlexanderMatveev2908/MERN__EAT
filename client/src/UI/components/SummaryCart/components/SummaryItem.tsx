0b702bf2f4ae16f1d39a6501c171ce13c7092703
Alex <alex@gmail.com>
2025-03-26 17:39:28 +0200

    _

--- client/src/UI/components/SummaryCart/components/SummaryItem.tsx
+++ client/src/UI/components/SummaryCart/components/SummaryItem.tsx
@@ -1,68 +1,26 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { X } from "lucide-react";
-import { FC, useEffect } from "react";
+import { FC } from "react";
 import { priceFormatter } from "../../../../utils/utils";
 import { calcTotPriceItem } from "../../../../utils/allUtils/priceFormatter";
 import { CartItem } from "../../../../types/allTypes/cart";
 import { useUpdateCart } from "../../../../core/hooks/cart/useUpdateCart";
 import MiniSpinner from "../../loaders/MiniSpinner";
-import { useForm } from "react-hook-form";
 import { fieldUpdateQty } from "../../../../core/config/fieldsArr/allFields/cart/update";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { updateQtyInputAPI } from "../../../../core/api/APICalls/cart";
-import { useToast } from "../../../../core/hooks/useGlobal";
-import { useHandleErr } from "../../../../core/hooks/useHandleErr";
-import { ErrFoodApp } from "../../../../types/allTypes/API";
 
 type PropsType = {
   item: CartItem;
 };
 
-type FormQtyType = {
-  quantity: string;
-};
-
 const SummaryItem: FC<PropsType> = ({ item }) => {
-  const queryClient = useQueryClient();
-
-  const { handleClickCart, isPending } = useUpdateCart({ dish: item });
-  const { showToastMsg } = useToast();
-  const { handleErrAPI } = useHandleErr();
-
-  const {
-    register,
-    setValue,
-    formState: { errors },
-    handleSubmit,
-  } = useForm<FormQtyType>({
-    mode: "onChange",
-    defaultValues: {
-      quantity: item.quantity + "",
-    },
+  const { handleClickCart, isPending, handlersInputQty } = useUpdateCart({
+    dish: item,
   });
-
-  useEffect(() => {
-    setValue("quantity", item.quantity + "");
-  }, [item, setValue]);
-
-  const { isPending: isPendingInputQTy, mutate } = useMutation({
-    mutationFn: (quantity: string) =>
-      updateQtyInputAPI({ dishId: item.dishId as string, quantity }),
-    onSuccess: () => {
-      showToastMsg("Cart updated", "SUCCESS");
-      queryClient.resetQueries({ queryKey: ["myCart"] });
-    },
-    onError: (err: ErrFoodApp) => {
-      handleErrAPI({ err });
-      setValue("quantity", item.quantity + "");
-    },
-  });
-
-  const changeQty = handleSubmit((data) => mutate(data.quantity));
 
   return (
     <li className="w-full grid gap-y-1 items-center md:grid-cols-2 gap-10">
       <form
-        onSubmit={changeQty}
+        onSubmit={handlersInputQty.changeQtyInput}
         className="w-full flex gap-5 justify-between items-center"
       >
         <span className="txt__02">{item.name}</span>
@@ -73,7 +31,7 @@ const SummaryItem: FC<PropsType> = ({ item }) => {
           step="any"
           type="number"
           className="txt__02 border-orange-500 border-2 outline-none rounded-xl focus__base el__flow px-3 py-[0.1rem] max-w-[100px] md:justify-self-start"
-          {...register(fieldUpdateQty.field as keyof FormQtyType, {
+          {...handlersInputQty.register(fieldUpdateQty.field as any, {
             pattern: {
               value: fieldUpdateQty.reg,
               message: fieldUpdateQty.msg,
@@ -91,7 +49,7 @@ const SummaryItem: FC<PropsType> = ({ item }) => {
           {calcTotPriceItem(item)}
         </span>
 
-        {isPending || isPendingInputQTy ? (
+        {isPending || handlersInputQty.isPendingInputQTy ? (
           <div className="justify-self-end">
             <MiniSpinner />
           </div>
@@ -106,8 +64,10 @@ const SummaryItem: FC<PropsType> = ({ item }) => {
         )}
       </div>
 
-      {errors?.quantity && (
-        <span className="txt__01 text-red-600">{errors.quantity.message}</span>
+      {handlersInputQty.errors?.quantity && (
+        <span className="txt__01 text-red-600">
+          {handlersInputQty.errors.quantity.message}
+        </span>
       )}
     </li>
   );
