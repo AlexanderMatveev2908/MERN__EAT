95682114fbf7492e660be0bb75648f75257dddf6
Alex <alex@gmail.com>
2025-03-28 12:32:34 +0200

    _

--- practise/SDA/REACT/src/components/buttons/ButtonBasic.jsx
+++ practise/SDA/REACT/src/components/buttons/ButtonBasic.jsx
@@ -1,4 +1,6 @@
-const ButtonBasic = ({ styleProp }) => {
+import PropTypes from "prop-types";
+
+const ButtonBasic = ({ styleProp, label, cbTextUser }) => {
   const handleStyle = () =>
     styleProp === 1
       ? "btn__1"
@@ -8,6 +10,16 @@ const ButtonBasic = ({ styleProp }) => {
       ? "btn__3"
       : "";
 
-  return <button className={`btn__base ${handleStyle()}`}>X</button>;
+  return (
+    <button onClick={cbTextUser} className={`btn__base ${handleStyle()}`}>
+      {label}
+    </button>
+  );
 };
+
+ButtonBasic.propTypes = {
+  styleProp: PropTypes.oneOf([1, 2, 3]).isRequired,
+  label: PropTypes.string.isRequired,
+};
+
 export default ButtonBasic;
