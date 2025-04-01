3bc0868b092bcb6a2f9e89824e97be0b3ff6eac6
Alexander Matveev <matveevalexander470@gmail.com>
2025-04-01 21:02:34 +0300

    _

--- client/src/core/context/global/GlobalProvider.tsx
+++ client/src/core/context/global/GlobalProvider.tsx
@@ -1,17 +1,14 @@
 import { FC, ReactElement, ReactNode } from "react";
 import { GlobalContext } from "./GlobalContext";
 import { useRootVals } from "../root/useRootVals";
-import { loadStripe } from "@stripe/stripe-js";
 
 type PropsType = {
   children: ReactNode | ReactNode[];
 };
 
-const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_KEY!);
-
 const GlobalProvider: FC<PropsType> = ({ children }): ReactElement => {
   return (
-    <GlobalContext.Provider value={{ ...useRootVals(), stripePromise }}>
+    <GlobalContext.Provider value={{ ...useRootVals() }}>
       {children}
     </GlobalContext.Provider>
   );
