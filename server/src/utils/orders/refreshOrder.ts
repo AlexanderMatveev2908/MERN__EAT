4e0ad04153d627eb8676174f15ca9ae8d01f6a1e
Alexander Matveev <matveevalexander470@gmail.com>
2025-04-01 19:08:14 +0300

    _

--- server/src/utils/orders/refreshOrder.ts
+++ server/src/utils/orders/refreshOrder.ts
@@ -6,6 +6,7 @@ import { baseErrResponse } from "../baseErrResponse.js";
 import { makeMongoId } from "../dbPipeline/general.js";
 import { RequestWithUserId } from "./../../middleware/general/verifyAccessToken.js";
 import Dish, { DishType } from "../../models/Dish.js";
+import Coupon, { CouponType } from "../../models/Coupon.js";
 
 export const checkIsOpen = (rest: RestaurantType) => {
   let isOpen = true;
@@ -88,4 +89,39 @@ export const getFreshItemsStock = async (order: OrderType) => {
   );
 
   return { orderItemsFresh, oldQty, newQty };
+};
+
+export const handleCouponOrder = async (
+  coupon: CouponType,
+  newTotPrice: number,
+  orderItemsFresh: OrderItem[]
+) => {
+  let resetCoupon = false;
+  let expiredCoupon = false;
+  let discount = 0;
+
+  const isPriceCOndOk = newTotPrice >= coupon.minCartPrice;
+  const isStillValid = new Date(coupon.expiryDate).getTime() > Date.now();
+
+  if ((!isPriceCOndOk || !orderItemsFresh.length) && isStillValid) {
+    resetCoupon = true;
+
+    await Coupon.findByIdAndUpdate(coupon._id, {
+      isActive: true,
+    });
+  } else if (!isStillValid) {
+    expiredCoupon = true;
+
+    await Coupon.findByIdAndUpdate(coupon._id, {
+      isActive: false,
+    });
+  } else {
+    discount = +((newTotPrice / 100) * coupon.discount).toFixed(2);
+  }
+
+  return {
+    resetCoupon,
+    expiredCoupon,
+    discount,
+  };
 };
