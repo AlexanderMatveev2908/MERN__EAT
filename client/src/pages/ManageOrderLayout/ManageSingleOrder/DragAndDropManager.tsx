5d8f7fe9f268c147834789fb8f1bea9c3222fee2
Alex <alex@gmail.com>
2025-04-05 23:01:30 +0300

    _

---
+++ client/src/pages/ManageOrderLayout/ManageSingleOrder/DragAndDropManager.tsx
@@ -0,0 +1,71 @@
+import { FC, useState } from "react";
+import { fieldsDragDrop } from "../../../core/config/fieldsArr/allFields/manageOrders/show";
+import { OrderType } from "../../../types/types";
+import { Ham } from "lucide-react";
+import { OrderStatusType } from "../../../types/allTypes/orders";
+
+type PropsType = {
+  order: OrderType;
+};
+
+const DragAndDropManager: FC<PropsType> = ({ order }) => {
+  const [status, setStatus] = useState(order.status);
+  const [isDragging, setIsDragging] = useState(false);
+
+  const iStatus = fieldsDragDrop.findIndex((el) => el.field === status);
+
+  const handleDragStart = (e: React.DragEvent, field: string) =>
+    e.dataTransfer.setData("status", field);
+  const handleDrop = (e: React.DragEvent, field: string) => {
+    const draggedStatus = e.dataTransfer.getData("status");
+    // i do not check cause i am pretty sure i will find an el inside an array sync and not fetched
+    const draggedStatusI = fieldsDragDrop.findIndex(
+      (el) => el.field === draggedStatus
+    );
+
+    e.currentTarget.classList.remove("el__drag_over");
+    setIsDragging(false);
+
+    const newStatusI = fieldsDragDrop.findIndex((el) => el.field === field);
+    if (newStatusI <= draggedStatusI) return;
+
+    setStatus(field as OrderStatusType);
+  };
+  const allowDrop = (e: React.DragEvent) => {
+    e.preventDefault();
+    setIsDragging(true);
+    e.currentTarget.classList.add("el__drag_over");
+  };
+  const handleDragLeave = (e: React.DragEvent) =>
+    e.currentTarget.classList.remove("el__drag_over");
+
+  console.log(status);
+
+  return (
+    <div className="w-full grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))] gap-x-10 gap-y-5">
+      {fieldsDragDrop.map((el, i) => (
+        <div
+          onDragOver={allowDrop}
+          onDragLeave={handleDragLeave}
+          onDrop={(e) => handleDrop(e, el.field)}
+          key={el.id}
+          className="border-2 border-[#333] rounded-xl px-6 py-2 flex justify-center items-center relative el__flow"
+        >
+          {i === iStatus && (
+            <div
+              draggable
+              onDragStart={(e) => handleDragStart(e, el.field)}
+              className={`bg-[#111] border-2 border-orange-500 text-orange-500 rounded-xl absolute right-0 bottom-1/2 p-2 el__flow hover:scale-110 cursor-pointer ${
+                isDragging ? "opacity-50" : "opacity-100"
+              }`}
+            >
+              <Ham className="min-w-[35px] min-h-[35px] " />
+            </div>
+          )}
+          <span className="txt__02">{el.label}</span>
+        </div>
+      ))}
+    </div>
+  );
+};
+export default DragAndDropManager;
--- client/src/pages/ManageOrderLayout/ManageSingleOrder/ManageSingleOrder.tsx
+++ client/src/pages/ManageOrderLayout/ManageSingleOrder/ManageSingleOrder.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect, useState } from "react";
+import { FC, useEffect } from "react";
 import { useScrollTop } from "../../../core/hooks/UI/useScrollTop";
 import { useQuery } from "@tanstack/react-query";
 import { useParams } from "react-router-dom";
@@ -13,17 +13,12 @@ import { showPairsUserInfo } from "../../../core/config/fieldsArr/allFields/mana
 import DropElStatic from "../../../UI/components/DropElStatic";
 import ContentMath from "../../../UI/components/ContentMath";
 import { getTotOrder } from "../../../utils/allUtils/priceFormatter";
-import {
-  getColorTimer,
-  getPercDelTime,
-} from "../../../utils/allUtils/formatTime";
-import { OrderType } from "../../../types/types";
 import { showFieldsDelivery } from "../../../core/config/fieldsArr/allFields/myOrders/show";
 import { IDPopulatedOrder } from "../../../types/allTypes/orders";
+import TimerDel from "./TimerDel";
+import DragAndDropManager from "./DragAndDropManager";
 
 const ManageSingleOrder: FC = () => {
-  const [percDel, setPercDel] = useState(0);
-
   const orderId = useParams()?.orderId;
   const canStay = REG_MONGO.test(orderId ?? "");
 
@@ -42,89 +37,57 @@ const ManageSingleOrder: FC = () => {
 
   const { order } = data ?? {};
 
-  useEffect(() => {
-    if (!isObjOk(order)) return;
-    const interval = setInterval(() => {
-      setPercDel(getPercDelTime(order as OrderType));
-    }, 1000);
-
-    return () => clearInterval(interval);
-  }, [order]);
-
   return (
     <ParentContentLoading {...{ isPending, isError, error, canStay }}>
       {order && isObjOk(order) && (
-        <>
+        <div className="w-full grid justify-items-center gap-6">
           <span className="txt__04">{order.restaurantName}</span>
+          <ListItemsOrder {...{ order }} />
 
-          <div className="w-full grid justify-items-center gap-6">
-            <ListItemsOrder {...{ order }} />
-
-            <div className="w-full grid grid-rows-1 gap-x-10 gap-y-5 items-start md:grid-cols-2">
-              {showPairsUserInfo(
-                Object.entries(order.infoUser) as string[][],
-                Object.entries(order.addressUser) as string[][]
-              ).map((el) => (
-                <DropElStatic {...{ el }}>
-                  {el.pairs.map((el) => (
-                    <li
-                      key={el.id}
-                      className="w-full grid grid-cols-[75px_1fr] gap-5"
-                    >
-                      <span className="txt__01">{el.pair[0]}</span>
-                      <span className="txt__01 justify-self-end block overflow-x-scroll hide_scrollbar max-w-full">
-                        {el.pair[1]}
-                      </span>
-                    </li>
-                  ))}
-                </DropElStatic>
-              ))}
-            </div>
-
-            <div className="w-full">
-              <ContentMath {...{ order, totOrder: getTotOrder(order) }} />
-            </div>
-
-            <ul className="w-full grid items-center gap-2">
-              {showFieldsDelivery(
-                order.timeConfirmed,
-                (order.restaurantId as IDPopulatedOrder).delivery
-                  .estTimeDelivery
-              ).map((el) => (
-                <li
-                  key={el.id}
-                  className="w-full grid grid-cols-2 items-center"
-                >
-                  <span className="txt__01">{el.label}</span>
-
-                  <span className="txt__01 justify-self-end sm:justify-self-center">
-                    {el.val}
-                  </span>
-                </li>
-              ))}
-            </ul>
-
-            {!["pending", "cancelled"].includes(order.status) && (
-              <div className="w-full relative mt-10">
-                <div className="w-full absolute h-[40px] border-2 border-orange-500  rounded-full"></div>
-
-                <div
-                  style={{ left: `calc(${percDel}% - 42.5px)` }}
-                  className="absolute border-2 h-[40px] w-[40px] rounded-full bg-[#111] z-60"
-                ></div>
-                <div className="absolute rounded-full h-[40px] w-full flex justify-start items-center p-1">
-                  <span
-                    style={{ width: `${percDel}%` }}
-                    className={`h-full rounded-full ${getColorTimer(
-                      order,
-                      percDel
-                    )}`}
-                  ></span>
-                </div>
-              </div>
-            )}
+          <div className="w-full grid grid-rows-1 gap-x-10 gap-y-5 items-start md:grid-cols-2">
+            {showPairsUserInfo(
+              Object.entries(order.infoUser) as string[][],
+              Object.entries(order.addressUser) as string[][]
+            ).map((el) => (
+              <DropElStatic {...{ el }}>
+                {el.pairs.map((el) => (
+                  <li
+                    key={el.id}
+                    className="w-full grid grid-cols-[75px_1fr] gap-5"
+                  >
+                    <span className="txt__01">{el.pair[0]}</span>
+                    <span className="txt__01 justify-self-end block overflow-x-scroll hide_scrollbar max-w-full">
+                      {el.pair[1]}
+                    </span>
+                  </li>
+                ))}
+              </DropElStatic>
+            ))}
           </div>
-        </>
+
+          <div className="w-full">
+            <ContentMath {...{ order, totOrder: getTotOrder(order) }} />
+          </div>
+
+          <ul className="w-full grid items-center gap-2">
+            {showFieldsDelivery(
+              order.timeConfirmed,
+              (order.restaurantId as IDPopulatedOrder).delivery.estTimeDelivery
+            ).map((el) => (
+              <li key={el.id} className="w-full grid grid-cols-2 items-center">
+                <span className="txt__01">{el.label}</span>
+
+                <span className="txt__01 justify-self-end sm:justify-self-center">
+                  {el.val}
+                </span>
+              </li>
+            ))}
+          </ul>
+
+          <TimerDel {...{ order }} />
+
+          <DragAndDropManager {...{ order }} />
+        </div>
       )}
     </ParentContentLoading>
   );
